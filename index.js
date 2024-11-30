413556cdef8160a72c16001b6fc020d43bd87b4f
Joseph Lau <lau.joseph.ca@gmail.com>
2024-11-30 02:51:14 -0800

    edit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById("btn")
 const pw1 = document.getElementById("pw1")
 const pw2 = document.getElementById("pw2")
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+const length = document.getElementById("length")
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
 
 function generatePw() {
     let password = ""
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < length.value; i++) {
         let randomIndex = Math.floor(Math.random() * characters.length)
         password += characters[randomIndex]
     }
